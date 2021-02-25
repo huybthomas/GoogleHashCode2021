c1c9ed59aa902c5559cae8c681dda7b4700adc0c
Steven de Cleene <decleene.steven@gmail.com>
2021-02-25 19:07:30 +0100

    Input, InputParse, input files

--- hashcode2021/src/main/java/com/stevecorp/codecontest/hashcode/hashcode2021/component/Input.java
+++ hashcode2021/src/main/java/com/stevecorp/codecontest/hashcode/hashcode2021/component/Input.java
@@ -5,14 +5,65 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 @Setter
 @Builder(toBuilder = true)
 public class Input implements InputModel {
 
+    public int simulationDurationSeconds;
+    public int numberOfIntersections;
+    public int numberOfStreets;
+    public int numberOfCars;
+    public int bonusPointsForReachingDestinationInTime;
+    public List<Street> streets;
+    public List<CarPath> carPaths;
+
+    @Getter
+    @Setter
+    @Builder(toBuilder = true)
+    public static final class Street {
+
+        public int streetId;
+        public int startIntersection;
+        public int endIntersection;
+        public int timeToGetFromStartToEnd;
+
+        public Street(final int streetId, final int startIntersection, final int endIntersection, final int timeToGetFromStartToEnd) {
+            this.streetId = streetId;
+            this.startIntersection = startIntersection;
+            this.endIntersection = endIntersection;
+            this.timeToGetFromStartToEnd = timeToGetFromStartToEnd;
+        }
+
+    }
+
+    @Getter
+    @Setter
+    @Builder(toBuilder = true)
+    public static final class CarPath {
+
+        public int numberOfStreets;
+        public List<Integer> streetIds;
+
+        public CarPath(final int numberOfStreets, final List<Integer> streetIds) {
+            this.numberOfStreets = numberOfStreets;
+            this.streetIds = streetIds;
+        }
+
+    }
+
     @Override
     public Input cloneInput() {
         return this.toBuilder()
+                .streets(this.streets.stream()
+                        .map(street -> street.toBuilder().build())
+                        .collect(Collectors.toList()))
+                .carPaths(this.carPaths.stream()
+                        .map(carPath -> carPath.toBuilder().build())
+                        .collect(Collectors.toList()))
                 .build();
     }
 
--- hashcode2021/src/main/java/com/stevecorp/codecontest/hashcode/hashcode2021/component/InputParserImpl.java
+++ hashcode2021/src/main/java/com/stevecorp/codecontest/hashcode/hashcode2021/component/InputParserImpl.java
@@ -2,13 +2,57 @@ package com.stevecorp.codecontest.hashcode.hashcode2021.component;
 
 import com.stevecorp.codecontest.hashcode.facilitator.configurator.input.InputParser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InputParserImpl implements InputParser<Input> {
 
     @Override
     public Input parseInput(final List<String> input) {
+
+        final int[] inputHeading = Arrays.stream(input.get(0).split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int currentId = 0;
+        final Map<String, Integer> streetIdMapping = new HashMap<>();
+        final List<Input.Street> streets = new ArrayList<>();
+        for(final String streetInput : input.subList(1, inputHeading[2] + 1)) {
+            final String[] streetInputElements = streetInput.split(" ");
+            final int startIntersection = Integer.parseInt(streetInputElements[0]);
+            final int endIntersection = Integer.parseInt(streetInputElements[1]);
+            final String streetName = streetInputElements[2];
+            final int timeToGetFromStartToEnd = Integer.parseInt(streetInputElements[3]);
+            if (!streetIdMapping.containsKey(streetName)) {
+                streetIdMapping.put(streetName, currentId++);
+            }
+            final int streetId = streetIdMapping.get(streetName);
+            streets.add(new Input.Street(streetId, startIntersection, endIntersection, timeToGetFromStartToEnd));
+        }
+
+        final List<Input.CarPath> carPaths = new ArrayList<>();
+        for(final String carPathInput : input.subList(1 + inputHeading[2] + 1, input.size())) {
+            final String[] carPathInputElements = carPathInput.split(" ");
+            final int numberOfStreets = Integer.parseInt(carPathInputElements[0]);
+            final List<Integer> streetIds = new ArrayList<>();
+            for (int streetIndex = 1; streetIndex < numberOfStreets; streetIndex++) {
+                final String streetName = carPathInputElements[streetIndex];
+                streetIds.add(streetIdMapping.get(streetName));
+            }
+            carPaths.add(new Input.CarPath(numberOfStreets, streetIds));
+        }
+
         return Input.builder()
+                .simulationDurationSeconds(inputHeading[0])
+                .numberOfIntersections(inputHeading[1])
+                .numberOfStreets(inputHeading[2])
+                .numberOfCars(inputHeading[3])
+                .bonusPointsForReachingDestinationInTime(inputHeading[4])
+                .streets(streets)
+                .carPaths(carPaths)
                 .build();
     }
 
